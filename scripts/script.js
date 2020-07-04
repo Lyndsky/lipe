529f2e63068774afea34bce2cae1676b9f961856
Marcos <marcosdoriguetto@gmail.com>
2020-07-04 19:33:26 -0300

    Correcao do get API Correcao do bug dos id's

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+function dado() {
+    fetch("https://reqres.in/api/users").then(res => res.json()).then(dados => {
+        const dadosData = dados.data;
+        for (let i = 0; i < dadosData.length; i++) {
+            const getTbody = document.querySelector("tbody");
+            const createTr = document.createElement("tr");
+            const createTh = document.createElement("th");
+            createTh.setAttribute("scope", "row");
+            const createTextTh = document.createTextNode(i + 1);
+            createTh.appendChild(createTextTh);
+            createTr.appendChild(createTh);
+            getTbody.appendChild(createTr);
+
+            for(let j = 1; j < 6; j++) {
+                const createTd = document.createElement("td");
+                const createTextTd = document.createTextNode("");
+                let allDados = dados.data[i];
+
+                if (j == 1) {
+                    createTextTd.textContent = allDados.id;
+                } else if (j == 2) {
+                    createTextTd.textContent = allDados.email;
+                } else if (j == 3) {
+                    createTextTd.textContent = allDados.first_name;
+                } else if (j == 4) {
+                    createTextTd.textContent = allDados.last_name;
+                } else {
+                    const createLink = document.createElement("a");
+                    createLink.style.transition = "0.3s ease-in";
+
+                    const createIcon = document.createElement("i");
+                    createIcon.setAttribute("class", "fab fa-buromobelexperte");
+
+                    if (allDados.first_name.length < 5) {
+                        const createIcon = document.createElement("i");
+                        createIcon.setAttribute("class", "fas fa-apple-alt");
+                        createTd.appendChild(createIcon);
+                    } else {
+                        createLink.setAttribute("href", allDados.avatar);
+                        createLink.setAttribute("class", "avatar");
+
+                        const createIcon = document.createElement("i");
+                        createIcon.setAttribute("class", "fab fa-apple");
+
+                        createIcon.style.fontSize = "30px";
+                        createTd.appendChild(createIcon);
+                    }
+
+                    createLink.appendChild(createIcon);
+                    createTd.appendChild(createLink);
+                }
+                createTd.appendChild(createTextTd);
+                createTr.appendChild(createTd);
+                getTbody.appendChild(createTr);
+            }    
+        }
+    } );
+}
+
+dado();
